4afff158ea4c06c5ea86fba3980f5777e3e04993
didier3787 <149539436+didier8737@users.noreply.github.com>
2023-10-31 23:30:11 +0100

    Add files via upload

---
+++ u =4.py
@@ -0,0 +1,14 @@
+u =4
+c = [1,5,8,9]
+for u in c:
+    print(u*4)
+
+b = {'papa': 49}
+val = b['papa']
+print(val)
+
+
+import time
+a = time.localtime()
+print ("il est" ,a.tm_hour, " heures et ", a.tm_min, " minutes et nous sommes le " ,a.tm_mday, " du mois ", a.tm_mon, " de l'année, ", a.tm_year)
+print ("hello world")
